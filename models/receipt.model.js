34da0a83e9a4a65a533e08e42cfc12f049790007
Amelia Salgado <amelie@MacBook-Pro-de-Amelia.local>
2018-02-03 15:43:23 +0100

    Feat: Receipt model.Ref #2

---
+++ models/receipt.model.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required']
+    },
+    ingredients: {
+        type: [
+            {  
+                ingredient: {
+                    type: String
+                },
+                amout: {
+                    type: Number
+                },
+                unit: {
+                    type: String
+                }
+                
+            }
+        ],
+        required: [true, 'Ingredients are required']
+    },
+    imgs: {
+        type: [
+            {
+                type: String
+            }
+        ]
+    },
+    rating: {
+        type: [
+            {
+                type: Number
+            }
+        ]
+    },
+    author: {
+        type: 
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            }
+    },
+    url: {
+        type: String
+    }
+}, { timestamps: true });
+
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    if (user.isAdmin()) {
+        user.role = 'BOSS';
+    }
+
+    bcrypt.genSalt(SALT_WORK_FACTOR)
+        .then(salt => {
+            bcrypt.hash(user.password, salt)
+                .then(hash => {
+                    user.password = hash;
+                    next();
+                })
+        })
+        .catch(error => next(error));
+});
+
+
+const User = mongoose.model('Receipt', userSchema);
+module.exports = User;
