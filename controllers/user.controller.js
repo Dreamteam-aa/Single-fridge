8e1eb35fbeed831f0e6416caca6426b8cdac3a8b
Antonella Briatore <antobria@gmail.com>
2018-02-13 20:49:08 +0100

    refactorization Amelia's big function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,9 +5,8 @@ require('isomorphic-fetch');
 var fs = require('fs');
 var path = require('path');
 var prompt = require('prompt');
-const ACCESS_TOKEN = "2iy2FNkeNWYAAAAAAAAA6E2nrYYL--f3MkSAtvn72g-4sDHe1qW51QJRCOxPtu6y";
-var dbx = new Dropbox({ accessToken: ACCESS_TOKEN });
 
+const dbx = require ('../config/dropbox.config');
 
 module.exports.profile = (req, res, next) => {
     Recipes.find({author: req.user._id})
@@ -56,41 +55,8 @@ module.exports.doEdit = (req, res, next) => {
         description: req.body.description
     };
     User.findByIdAndUpdate(userId, updates).then((user) => {
-        uploadDB(img,user);
+        dbx.uploadDB(img,user);
       res.redirect('/profile');
     });
 };
 
-function uploadDB(filePath,updatedProfile){
-    path = "./public/uploads/" + filePath;
-    fs.readFile(path, function (err, contents) {
-        if (err) {
-          console.log('Error: ', err);
-        } 
-        // This uploads basic.js to the root of your dropbox
-        dbx.filesUpload({ path: '/' + filePath, contents: contents })
-          .then(function (response) {
-            parameters = {
-                    "path": response.path_lower,
-                    "settings": {
-                        "requested_visibility": "public"
-                }
-            };
-            dbx.sharingCreateSharedLinkWithSettings(parameters)
-            .then(response => {
-                urlAux = response.url.split("/s");
-                url = urlAux[1].split("?");
-                console.log(url);
-                User.findByIdAndUpdate(updatedProfile, { profileImg: url[0] }, { new: true })
-                                .then((user) => console.log(user));
-               // return response.url;
-            })
-            .catch(function (err) {
-                console.log(err);
-              });
-          })
-          .catch(function (err) {
-            console.log(err);
-          });
-      });
-    }
